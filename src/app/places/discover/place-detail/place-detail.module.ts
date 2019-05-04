98f97a5690eaf79a89b6a5c9606a288666cd7a4e
Zinzidane <ya-gans@mail.ru>
2019-05-04 21:36:00 +0300

    Add dummy modal for ordering

--- src/app/places/discover/place-detail/place-detail.module.ts
+++ src/app/places/discover/place-detail/place-detail.module.ts
@@ -5,6 +5,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { PlaceDetailPage } from './place-detail.page';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
 
 const routes: Routes = [
   {
@@ -19,6 +20,7 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [PlaceDetailPage]
+  declarations: [PlaceDetailPage, CreateBookingComponent],
+  entryComponents: [CreateBookingComponent]
 })
 export class PlaceDetailPageModule {}
--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { NavController, ModalController } from '@ionic/angular';
+import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
 
 @Component({
   selector: 'app-place-detail',
@@ -9,12 +10,17 @@ import { NavController } from '@ionic/angular';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor(private router: Router, private navCtlr: NavController) { }
+  constructor(private router: Router, private navCtlr: NavController, private modalCtrl: ModalController) { }
 
   ngOnInit() {
   }
 
   onBookPlace() {
-    this.navCtlr.navigateBack('/places/tabs/discover');
+    // this.navCtlr.navigateBack('/places/tabs/discover');
+    this.modalCtrl.create({
+      component: CreateBookingComponent
+    }).then(modalEl => {
+      modalEl.present();
+    });
   }
 }
