31c00006607052def44628cc37c43c0bdf8c7750
Zinzidane <ya-gans@mail.ru>
2019-05-05 17:07:27 +0300

    Add styling

--- src/app/places/offers/offers.module.ts
+++ src/app/places/offers/offers.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
 import { OffersPage } from './offers.page';
-import { Routes, RouterModule } from '@angular/router';
+import { OfferItemComponent } from './offer-item/offer-item.component';
 
 const routes: Routes = [
   {
@@ -16,9 +18,10 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [OffersPage]
+  declarations: [OffersPage, OfferItemComponent]
 })
 export class OffersPageModule {}
--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,21 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { PlacesService } from '../places.service';
-import { Place } from '../place.model';
 import { IonItemSliding } from '@ionic/angular';
 import { Router } from '@angular/router';
+
+import { PlacesService } from '../places.service';
+import { Place } from '../place.model';
 
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.page.html',
-  styleUrls: ['./offers.page.scss'],
+  styleUrls: ['./offers.page.scss']
 })
 export class OffersPage implements OnInit {
   offers: Place[];
 
-  constructor(
-    private placesService: PlacesService,
-    private router: Router
-  ) { }
+  constructor(private placesService: PlacesService, private router: Router) {}
 
   ngOnInit() {
     this.offers = this.placesService.places;
@@ -24,6 +22,6 @@ export class OffersPage implements OnInit {
   onEdit(offerId: string, slidingItem: IonItemSliding) {
     slidingItem.close();
     this.router.navigate(['/', 'places', 'tabs', 'offers', 'edit', offerId]);
-    console.log('Edited item', offerId);
+    console.log('Editing item', offerId);
   }
 }
