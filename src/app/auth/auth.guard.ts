403b749f33f5634da17aa56176656d1f5a3829a2
Zinzidane <ya-gans@mail.ru>
2019-05-05 19:59:09 +0300

    Finish auth form

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanLoad, Route, UrlSegment, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -9,11 +10,13 @@ import { AuthService } from './auth.service';
 export class AuthGuard implements CanLoad {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ): Observable<boolean> | Promise<boolean> | boolean {
     if (!this.authService.userIsAuthenticated) {
-      return this.router.navigateByUrl('/auth');
+      this.router.navigateByUrl('/auth');
     }
     return this.authService.userIsAuthenticated;
   }
-
 }
