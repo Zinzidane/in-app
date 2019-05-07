7f6de023d476b1696647c04fe43732d1278fb2c7
Zinzidane <ya-gans@mail.ru>
2019-05-07 12:08:46 +0300

    Implement fetch single place in firebase

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -57,12 +57,22 @@ export class PlacesService {
   }
 
   getPlace(id: string) {
-    return this.places.pipe(
-      take(1),
-      map(places => {
-        return { ...places.find(p => p.id === id) };
-      })
-    );
+    return this.http
+      .get<PlaceData>(`https://ionic-app-876a0.firebaseio.com/offered-places/${id}.json`)
+      .pipe(
+        map(placeData => {
+          return new Place(
+            id,
+            placeData.title,
+            placeData.description,
+            placeData.imageUrl,
+            placeData.price,
+            new Date(placeData.availableFrom),
+            new Date(placeData.availableTo),
+            placeData.userId
+          );
+        })
+      )
   }
 
   addPlace(
